534662235e7902db35f19565583c8125d798de7c
fall lewis Datascientest <fall-lewis.y@datascientest.com>
2025-11-01 21:56:33 +0000

    updated API message after Jenkins config review!

--- app/main.py
+++ app/main.py
@@ -5,4 +5,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "We Love Datascientest, and we did it. We built a Complete CI/CD Pipeline, how ecxiting !"}
+    return {"Hello": "We Love Datascientest, and we did it. We built a Complete CI/CD Pipeline, how ecxiting, with a few mods from myself to force pushing latest version!"}
